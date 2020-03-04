bc6cf02312b15d22a9d6eb42a40de5af6eefe06f
Tristan Grovender <therealtristangrovender@gmail.com>
2020-03-04 15:33:53 -0600

    MVP Complete. No Stretch

--- index.js
+++ index.js
@@ -92,7 +92,17 @@ Car.prototype.fill = function(gallons) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {}
+function Baby(name, age, favoriteToy) {
+    // Brings in all the Person attributes
+    Person.call(this, name, age);
+    this.favoriteToy = favoriteToy;
+}
+// This sets up the __proto__ to inherit the prototype from Person
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
